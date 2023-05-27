5997a74d602bbb388daeff9f75a0ff20268bc9b0
Timmy Vu <tv75@cornell.edu>
2023-05-27 14:45:27 -0400

    login page

---
+++ script.js
@@ -0,0 +1,15 @@
+function authenticate(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorText = document.getElementById("errorText");
+    
+    if (username === "may" && password === "iamhorny") {
+      // Successful authentication
+      window.location.href = "welcome.html"; // Redirect to a welcome page
+    } else {
+      // Display error message
+      errorText.textContent = "Invalid username or password.";
+    }
+  }
